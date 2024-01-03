a33e929786c6b865bee1b2c1e9ee54ffa0320cfb
Leo Hsu <leodream251324@gmail.com>
2024-01-03 12:59:47 +0800

    fix wrong absolute path

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,9 +1,6 @@
 from .blender import BlenderDataset
 from .llff import LLFFDataset
-import sys 
-print(sys.path[0])
-sys.path.append('/home/leohsu-cs/DLCV2023/hw4')
-from dataset import KlevrDataset
+from .dataset import KlevrDataset
 
 dataset_dict = {'blender': BlenderDataset,
                 'llff': LLFFDataset,
